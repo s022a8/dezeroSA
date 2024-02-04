625306f368fbae3728b24b8008f9433b60d6493b
sora <kafuka821shiro@gmail.com>
2024-02-04 14:08:46 +0900

    step1-10

---
+++ steps/step01.py
@@ -0,0 +1,22 @@
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+import numpy as np
+
+#---
+# data = np.array(1.0)
+# x = Variable(data)
+# print(x.data)
+
+# x.data = np.array(2.0)
+# print(x.data)
+
+#--
+# x = Variable(np.array(10))
+# f = Function()
+# y = f(x)
+
+# print(type(y))
+# print(x.data)
+# print(y.data)
---
+++ steps/step02.py
@@ -0,0 +1,21 @@
+from step01 import *
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+# x = Variable(np.array(10))
+# f = Square()
+# y = f(x)
+# print(type(y))
+# print(y.data)
