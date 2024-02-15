a8ae6ec47989a5eaeff2268c76f16fa66529ea07
sora <kafuka821shiro@gmail.com>
2024-02-16 00:36:20 +0900

    step29

---
+++ steps/step29.py
@@ -0,0 +1,30 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import math
+import numpy as np
+from dezero import Function
+from dezero import Variable
+from dezero.utils import plot_dot_graph
+
+def f(x):
+    y = x ** 4 - 2 * x ** 2
+    return y
+
+def gx2(x):
+    return 12 * x ** 2 - 4
+
+x = Variable(np.array(2.0))
+iters = 10
+
+for i in range(iters):
+    print(i, x)
+    
+    y = f(x)
+    x.cleangrad()
+    y.backward()
+    
+    x.data -= x.grad / gx2(x.data)
+
+
