01b898706baafb04827a621ae948cc387d61960d
sora <kafuka821shiro@gmail.com>
2024-02-15 15:42:46 +0900

    core_simple.py

--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -1,9 +1,9 @@
 import numpy as np
 import weakref
+import contextlib
 
-import sys
-sys.path.append('.')
-import config.config as cfg
+class Config:
+    enable_backprop = True
 
 class Variable:
     __array_priority = 200
@@ -96,7 +96,7 @@ class Function:
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
         
-        if cfg.Config.enable_backprop:
+        if Config.enable_backprop:
             self.generation = max([x.generation for x in inputs])
             for output in outputs:
                 output.set_creator(self)
@@ -110,26 +110,6 @@ class Function:
     
     def backward(self, gy):
         raise NotImplementedError()
-
-class Square(Function):
-    def forward(self, x):
-        y = x ** 2
-        return y
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
-    
-class Exp(Function):
-    def forward(self, x):
-        y = np.exp(x)
-        return y
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = np.exp(x) * gy
-        return gx
 
 class Add(Function):
     def forward(self, x0, x1):
@@ -188,11 +168,34 @@ class Pow(Function):
         gx = c * x ** (c - 1) * gy
         return gx
 
-def square(x):
-    return Square()(x)
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
 
-def exp(x):
-    return Exp()(x)
+def no_grad():
+    return using_config('enable_backprop', False)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
 
 def add(x0, x1):
     x1 = as_array(x1)
@@ -224,30 +227,14 @@ def neg(x):
 def pow(x, c):
     return Pow(c)(x)
 
-def as_array(x):
-    if np.isscalar(x):
-        return np.array(x)
-    return x
-
-def as_variable(obj):
-    if isinstance(obj, Variable):
-        return obj
-    return Variable(obj)
-
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
-
-Variable.__add__ = add
-Variable.__radd__ = add
-Variable.__sub__ = sub
-Variable.__rsub__ = rsub
-Variable.__mul__ = mul
-Variable.__rmul__ = mul
-Variable.__truediv__ = div
-Variable.__rtruediv__ = rdiv
-Variable.__neg__ = neg
-Variable.__pow__ = pow
+def setup_variable():
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__sub__ = sub
+    Variable.__rsub__ = rsub
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__neg__ = neg
+    Variable.__pow__ = pow
